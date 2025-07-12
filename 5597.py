61089e9b71c314872864a1d0d63ee9377ba7f01d
Shen-a <shinhodong2002@gmail.com>
2025-07-13 03:41:08 +0900

    Back UP

--- 5597.py
+++ 5597.py
@@ -1,3 +1,11 @@
+num = []
+for i in range(28):
+    num.append(int(input()))
 
+num.sort()
+num.append(0)
+num.append(0)
 
-input()
+for i in range(1, 31, 1):
+    if i not in num:
+        print(i)
